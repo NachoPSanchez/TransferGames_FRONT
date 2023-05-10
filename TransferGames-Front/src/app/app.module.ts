03ddf445f11dadfbd0fecd9f6d85c2b5a37a9e77
Marcos Retamero Orovio <marcosypablo123@gmail.com>
2023-05-10 18:02:47 +0200

    Update Header

--- TransferGames-Front/src/app/app.module.ts
+++ TransferGames-Front/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ComponentepruebaComponent } from './core/componenteprueba/componenteprueba.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentepruebaComponent } from './core/componenteprueba/componenteprueba.component';
+import { HeaderComponent } from './core/components/shared/header/header.component';
+import { FooterComponent } from './core/components/shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentepruebaComponent
+    ComponentepruebaComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
