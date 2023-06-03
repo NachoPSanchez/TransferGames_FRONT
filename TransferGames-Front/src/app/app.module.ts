cbc7c096ef38c29ff7b1489819f69b282f8f73c5
NachoPSanchez <nachopsanchez@gmail.com>
2023-06-03 21:00:54 +0200

    Fix footer & carousel home

--- TransferGames-Front/src/app/app.module.ts
+++ TransferGames-Front/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './shared/login/login.component';
-import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './shared/register/register.component';
-import { HttpClientModule } from '@angular/common/http';
 import { JuegosComponent } from './core/components/views/juegos/juegos.component';
-import { provideRouter } from '@angular/router';
 import { HomeComponent } from './core/components/views/home/home.component';
 import { ForoComponent } from './core/components/views/foro/foro.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 @NgModule({
   declarations: [
@@ -23,22 +25,17 @@ import { ForoComponent } from './core/components/views/foro/foro.component';
     RegisterComponent,
     JuegosComponent,
     HomeComponent,
-    ForoComponent
+    ForoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CarouselModule
   ],
-  providers: [
-    provideRouter([
-      {
-        path:'juegos', component: JuegosComponent
-      }
-    ])
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- TransferGames-Front/src/app/core/components/views/home/home.component.ts
+++ TransferGames-Front/src/app/core/components/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor() {}
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:1650,
+    autoplayHoverPause:true,
+    fluidSpeed: true,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      4080: {
+        items: 4
+      }
+    },
+    nav: false
+  }
+  
 }
