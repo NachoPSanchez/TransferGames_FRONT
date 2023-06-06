d6faaacc15ab2d66b030d7c4cbe4c99a48190f3a
Manuel Gutierrez <manueelguty@gmail.com>
2023-06-06 14:38:09 +0200

    Update Register Mensajes

--- TransferGames-Front/src/app/shared/register/register.component.ts
+++ TransferGames-Front/src/app/shared/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Credentials } from 'src/app/core/models/credentials';
 import { LoginCredentials } from 'src/app/core/models/loginCredentials';
 import { LoginService } from 'src/app/core/services/login.service';
@@ -21,22 +22,16 @@ export class RegisterComponent {
     roles:[]
   };
   
-  constructor(private registerService:RegisterService, private loginService: LoginService, private router: Router){}
+  constructor(private registerService:RegisterService, private loginService: LoginService, private router: Router, private toastr: ToastrService){}
 
   register(form: NgForm){
     if(form.valid){
       this.registerService.register(this.creds).subscribe(() =>{
-        Swal.fire({
-          title:`Hello, ${this.creds.name}`,
-          text:'Register successfully. Welcome to TransferGames Community',
-          icon:'success'
-        }).then(() =>{
-          const creds: LoginCredentials = {
-            email: this.creds.email,
-            password: this.creds.password
-          };
-          this.loginService.login(creds).subscribe(() =>{});
-          this.router.navigate(['/home']);
+        
+      }, (error)=>{
+        this.toastr.error(error.error.slice(7), 'Error', {
+          timeOut: 4000,
+          
         });
       });
     }else{
